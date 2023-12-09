8e822c8cce358832a017edcef3e5cd3495c599fe
vinit <vinitkumar4209@gmail.com>
2023-12-10 01:32:41 +0530

    new file

---
+++ JS By HC/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert (3+3) // we are using node.js , not browser 
+
+console.log(3+3)
+console.log("vinit")
+
+const score = 500; //number
+const scoreId = 500.2; //number
+const name = 'vinit';  //string
+const isCool = true;  //boolean
+const symbol = Symbol('id');  //symbol
+const n = null;  //object
+const u = undefined;  //undefined
+
+console.log(typeof(n));
+console.log(typeof(u));
+console.log(typeof(isCool));
+console.log(typeof(name));
+console.log(typeof(score));
+console.log(typeof(scoreId));
+console.log(typeof(symbol));
