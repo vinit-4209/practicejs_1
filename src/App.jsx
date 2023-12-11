76ef92e4e1e6c3262f1d59dd204740644d0afb77
vinit-4209 <vinitkumar4209@gmail.com>
2023-12-11 12:45:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import KgButton from "./KgButton";
+function App(){
+  return (
+    <div>
+      <h1>My first react app </h1>
+      <KgButton></KgButton>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/KgButton.jsx
@@ -0,0 +1,7 @@
+function KgButton(){
+    return (
+        <button>Submit the form</button>
+        // <h1>Submit  </h1>
+    )
+}
+export default KgButton;
